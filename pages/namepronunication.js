041847072610b098a0e4440194d6f0bfdc735297
MaiAnh Nguyen <hnguyen247@my.bcit.ca>
2022-05-19 21:46:33 -0700

    fixing again

--- pages/namepronunication.js
+++ pages/namepronunication.js
@@ -1,8 +1,6 @@
-import PronounciationCont from "../comps/pronunicationcont"; 
-import styles from '../styles/Home.module.css'
+import PronounciationCont from "../comps/pronunicationcont";
 import TopBar from '../comps/TopBar';
 import NavBar from '../comps/NavBar';
-import React, { useState } from 'react';
 import { PageWrap } from "../comps/SetComps";
 import { Names } from "../data/name_data.js";
 import { useRouter } from 'next/router';
@@ -11,14 +9,10 @@ export default function PronunciationPage() {
     const r = useRouter();
     var { page, index } = r.query;
 
-    // if (item === undefined) {
-    //     item = 0;
+    // if (page) {
+
+    //     console.log(Names[page][index])
     // }
-
-    if (page) {
-
-        console.log( Names[page][index])
-    }
 
 
     return <div>
@@ -26,20 +20,12 @@ export default function PronunciationPage() {
 
         <PageWrap>
             <TopBar backto='/namepageresult'></TopBar>
-            {/* <PronounciationCont nametxt={VietName[item].Name}></PronounciationCont> */}
-            {/* {(page) && Names.map((o, index) => (
-                <PronounciationCont nametxt={page} countryname={Names[item].Country}></PronounciationCont>
-            ))} */}
-
-        
-                <PronounciationCont nametxt={Names[page][index].Name}
-                countryname={Names[page][index].Lang} 
-                meaning={Names[page][index].Meaning} 
-                origin={Names[page][index].Origin} 
-                period={Names[page][index].Period} 
+            <PronounciationCont nametxt={Names[page][index].Name}
+                countryname={Names[page][index].Lang}
+                meaning={Names[page][index].Meaning}
+                origin={Names[page][index].Origin}
+                period={Names[page][index].Period}
                 pronunciation={Names[page][index].Pronunciation}></PronounciationCont>
-    
-
             <NavBar namebuttoncolor='#FC5F6C'
                 nametextcolor='#FC5F6C'>
             </NavBar>
